c35cb5a477a348c2c79b8fe2cfd9cab3ecf1596e
Jason Barrera <barrerajason81@gmail.com>
2020-02-06 08:50:19 -0600

    Added Networking class and data model

---
+++ StackX App/NetworkClient.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkClient.swift
+//  StackX App
+//
+//  Created by User on 2/3/20.
+//  Copyright © 2020 Jason B. All rights reserved.
+//
+
+import UIKit
+
+class NetworkClient: NSObject {
+
+}
